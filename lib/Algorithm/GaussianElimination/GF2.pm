5aa8252dc7a265776ee9b2ac6c2ce8d4ae3cca63
Salvador Fandiño García <salva@cpan.org>
2012-07-09 09:36:44 +0000

    Import of SALVA/Algorithm-GaussianElimination-GF2-0.02 from CPAN.
    gitpan-cpan-distribution: Algorithm-GaussianElimination-GF2
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         SALVA/Algorithm-GaussianElimination-GF2-0.02.tar.gz
    gitpan-cpan-author:       SALVA
    gitpan-cpan-maturity:     released

--- lib/Algorithm/GaussianElimination/GF2.pm
+++ lib/Algorithm/GaussianElimination/GF2.pm
@@ -1,6 +1,6 @@
 package Algorithm::GaussianElimination::GF2;
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 use strict;
 use warnings;
@@ -22,6 +22,8 @@ sub new_equation {
     $self->_add_equation($eq);
     $eq;
 }
+
+*add_equation = \&new_equation;
 
 sub _first_1 {
     pos($_[0]) = 0;
@@ -194,21 +196,21 @@ Algorithm::GaussianElimination::GF2 - Solve linear systems of equations on GF(2)
   use Algorithm::GaussianElimination::GF2;
 
   my $age = Algorithm::GaussianElimination::GF2->new;
-  $age->add_equation(1, 0, 0, 1 => 1);
-  $age->add_equation(0, 0, 1, 1 => 0);
+  $age->new_equation(1, 0, 0, 1 => 1);
+  $age->new_equation(0, 0, 1, 1 => 0);
   my ($sol, @base0) = $age->solve;
 
   # or you can also create the equations setting elements at given
   # positions:
 
   my $age = Algorithm::GaussianElimination::GF2->new;
-  my $eq1 = $age->add_equation;
+  my $eq1 = $age->new_equation;
   $eq1->a(0, 1);
   $eq1->a(3, 1);
   $eq1->b(1);
-  my $eq2 = $age->add_equation;
+  my $eq2 = $age->new_equation;
+  $eq2->a(2, 1);
   $eq2->a(3, 1);
-  $eq2->a(4, 1);
   $eq2->b(0);
   my ($sol, @base0) = $age->solve;
 
@@ -226,9 +228,9 @@ Those are the interesting methods:
 
 =item $age = Algorithm::GaussianElimination::GF2->new;
 
-=item $eq = $age->add_equation(@a, $b)
+=item $eq = $age->new_equation(@a, $b)
 
-=item $eq = $age->add_equation()
+=item $eq = $age->new_equation()
 
 Creates and adds a new equation to the algorithm.
 
@@ -302,7 +304,7 @@ for GF(2).
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (C) 2011 by Salvador FandiE<ntilde>o (sfandino@yahoo.com)
+Copyright (C) 2011, 2012 by Salvador FandiE<ntilde>o (sfandino@yahoo.com)
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself, either Perl version 5.14.2 or,
